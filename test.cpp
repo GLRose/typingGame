3e7214814f0fb9903ece8e2894f5f11ce3a9f0ec
glrose <garrett.rose03@gmail.com>
2020-09-01 21:37:02 -0400

    -

--- test.cpp
+++ test.cpp
@@ -1,39 +1,24 @@
-#include <iostream>
-#include <string>
-#include <vector>
-#include <fstream>
-using namespace std;
+// Compare name input to names in the txt file.. shows the index where found
 
+//     if (nameFile.is_open())
+// {
+//     while (nameFile >> here)
+//     {
+//         names.push_back(here);
+//         compare script if (firstName.compare(names[i]) == 0)
+//         {
+//             cout << "Match"
+//                  << " at index: " << i;
+//         }
+//         i++;
+//     }
+// }
+
+#include<time.h>
+#include<iostream>
+using namespace std;
 int main()
 {
-    //Array implementation
-    // string names[5];
 
-    // ifstream nameFile("names.txt");
 
-    // if (nameFile.is_open())
-    // {
-    //     for(int i=0; i < sizeof(names); i++)
-    //     {
-    //         nameFile >> names[i];
-    //         cout << names[i];
-    //     }
-    // }
-    //end
-
-    //Vector implementation
-    vector<string> names;
-
-    ifstream nameFile("names.txt");
-    string here;
-    int i = 0;
-    if (nameFile.is_open())
-    {
-        while(nameFile >> here)
-        {
-            names.push_back(here);
-            cout << names[i] << "\n";
-            i++;
-        }
-    }
 }
