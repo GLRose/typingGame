34eace045e4d267513d125ba8838d315e4982231
glrose <garrett.rose03@gmail.com>
2020-09-07 19:48:59 -0400

    Ideas and applications for timing our user input

--- test.cpp
+++ test.cpp
@@ -1,24 +1,25 @@
-// Compare name input to names in the txt file.. shows the index where found
 
-//     if (nameFile.is_open())
-// {
-//     while (nameFile >> here)
-//     {
-//         names.push_back(here);
-//         compare script if (firstName.compare(names[i]) == 0)
-//         {
-//             cout << "Match"
-//                  << " at index: " << i;
-//         }
-//         i++;
-//     }
-// }
-
-#include<time.h>
-#include<iostream>
-using namespace std;
-int main()
-{
-
-
-}
+// C++ program to illustrate the utility  
+// function duration::count 
+#include <iostream>  
+#include <chrono>     
+  
+int main () 
+{ 
+    using namespace std::chrono; 
+    // std::chrono::milliseconds is an  
+    // instantiation of std::chrono::duration:- 1 second 
+    milliseconds mil(1000);  
+      
+    mil = mil*60; 
+      
+    std::cout << "duration (in periods): "; 
+    std::cout << mil.count() << " milliseconds.\n"; 
+      
+    std::cout << "duration (in seconds): "; 
+    std::cout << (mil.count() * milliseconds::period::num /  
+                               milliseconds::period::den); 
+    std::cout << " seconds.\n"; 
+  
+    return 0; 
+} 
