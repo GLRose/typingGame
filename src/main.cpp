86974098dbcd7d41521cfa40fd80a1ee0094cd15
glrose <garrett.rose03@gmail.com>
2020-09-08 01:32:37 -0400

    Timer for input

--- src/main.cpp
+++ src/main.cpp
@@ -4,15 +4,22 @@
 #include <fstream>
 #include <time.h>
 #include <math.h>
+#include <chrono>
 
 using namespace std;
 
 int main()
 {
+    //timing input clock
+    using Clock = std::chrono::high_resolution_clock;
+
     int randomNum;
+    double randomNumTimer;
+    const int max = 5000;
+    const int min = 2500;
     srand(time(NULL));
     randomNum = rand() % 30 + 1;
-    //variable read into
+    randomNumTimer = rand()%(max-min + 1) + min;
     string firstName;
     string nameInput;
     string temp;
@@ -22,7 +29,7 @@ int main()
     ifstream nameFile("../txt_files/names.txt");
     string here;
     int i = 0;
-    int countdown = randomNum;//testing
+    int countdown = randomNum; //testing
     int correct = 0;
     int wrong = 0;
 
@@ -41,7 +48,17 @@ int main()
         cout << "Name: " << temp << "\n"
              << "Type the name quick!"
              << "\n";
+        auto start = Clock::now();
         cin >> nameInput;
+        auto end = Clock::now();
+        auto ms = std::chrono::duration_cast<std::chrono::milliseconds>(end - start).count();
+        std::cout << "Your input took " << ms << " milliseconds" << std::endl;
+
+        if(ms > randomNumTimer)
+        {
+            cout << "You lose" << " " << "the time limit was: " << " " << randomNumTimer << "ms" << endl;
+            return 0;
+        }
 
         if (nameInput.compare(temp) == 0)
         {
